7be28d2b4fc8f293971131d68c5299e5e464e97e
Diego leandro <diegolgaspar000@gmail.com>
2022-01-15 22:06:17 -0300

    atualizando o projeto

--- src/main/java/com/works/api/controller/ClienteController.java
+++ src/main/java/com/works/api/controller/ClienteController.java
@@ -4,26 +4,20 @@ import com.works.api.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clientes")
     public List<Cliente> listar(){
-      var cliente1 = new Cliente();
-      cliente1.setId(1L);
-      cliente1.setNome("Diego");
-      cliente1.setTelefone("34  23444-1111");
-      cliente1.setEmail("diegoleandro@ifood.com");
-
-        var cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Alice");
-        cliente2.setTelefone("21 5342-2444");
-        cliente2.setEmail("alicemoda@youtube.com");
-
-        return Arrays.asList(cliente1,cliente2);
+        return manager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 }
--- src/main/java/com/works/api/model/Cliente.java
+++ src/main/java/com/works/api/model/Cliente.java
@@ -1,16 +1,27 @@
 package com.works.api.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String email;
     private String telefone;
+
+
 
 
 }
